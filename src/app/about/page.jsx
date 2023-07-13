92d6f2a8197480c88965ef6130f1cedc7687920a
Musili Brian <musilibrian07@gmail.com>
2023-07-13 23:20:13 +0300

    about and blogs

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import aboutImage from "/public/3.jpg";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full">
+      {/* upper section */}
+      <div className=" relative w-full   h-[300px]">
+        <Image
+          src={aboutImage}
+          className="h-full w-full grayscale-[80%] object-cover"
+          alt="about"
+        />
 
-export default page
+        <div className="bg-secondary-100  px-6 py-2 absolute bottom-[20px] rounded-lg left-[30px] md:left-[60px] lg:left-[100px]">
+          <p className=" md:text-lg font-semibold tracking-widest">About us</p>
+        </div>
+      </div>
+
+      {/* lower section */}
+
+      <div className="w-5/6 mx-auto pt-10 pb-16 flex flex-col gap-8 md:gap-[100px] md:flex-row">
+        {/* item 1 */}
+        <div className="flex-1 justify-start items-center text-justify">
+          <h1 className="my-2 text-base md:text-lg font-semibold">Who we are</h1>
+          <p className="text-sm leading-[30px]">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo dolor
+            repellendus pariatur! Eos, eum, quo itaque labore assumenda deleniti
+            magni vitae placeat eligendi corrupti sunt ipsum nihil eius
+            voluptatem. Accusamus?
+          </p>
+        </div>
+
+        {/* item 2 */}
+        <div className="flex-1 justify-start items-center text-justify">
+          <h1 className="my-2 text-base md:text-lg font-semibold">What we do</h1>
+          <p className="text-sm leading-[30px]">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo dolor
+            repellendus pariatur! Eos, eum, quo itaque labore assumenda deleniti
+            magni vitae placeat eligendi corrupti sunt ipsum nihil eius
+            voluptatem. Accusamus?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default page;
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import benCarson from "public/2.jpg";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const blogs = [
+    {
+      image: benCarson,
+      title: "Take the risk",
+      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, tempore maiores sit quia est dignissimos animi vero saepe id excepturi?",
+    },
+  ];
 
-export default page
+  return (
+    <section className="w-full pt-10 pb-16">
+      <div className="w-5/6 mx-auto flex flex-col">
+        {blogs.map((blog, i) => (
+          <div className="flex flex-col md:flex-row gap-4 md:gap-[50px]" key={i}>
+            <div className="flex-1">
+              <Image
+                src={blog.image}
+                className="h-[200px] object-cover"
+                alt="image"
+              />
+            </div>
+
+            {/* item 2 */}
+
+            <div className="flex flex-1 flex-col">
+              <div className="flex mb-5 justify-between items-center">
+                <div>
+                  <p>{blog.title}</p>
+                </div>
+                <div>
+                  <p className="">User name</p>
+                </div>
+              </div>
+              <div>
+                <p className="text-sm">{blog.info}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default page;
